da1573d003cabf48ea5ebfef0cdf51a4d988824c
NTPhong <phongnguyenphong267@gmail.com>
2026-08-11 18:17:38 +0700

    chore - xoá .eslintrc.js legacy và rút gọn config Prettier
    eslint.config.js (flat config) mới là nguồn duy nhất có hiệu lực.

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-  // Ép ESLint phải báo lỗi đỏ nếu code không tuân thủ đúng định dạng Prettier ở trên
-  'prettier/prettier': 'error',
-};
